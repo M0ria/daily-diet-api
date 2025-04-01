e45412b848b28408e6c3353bf5068f1c63fcdc19
Maria <mariacastroferreira17@gmail.com>
2025-04-01 17:39:31 -0300

    Add global exception filter for handling HTTP errors

---
+++ src/infra/http/app.exceptions.filter.ts
@@ -0,0 +1,28 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch()
+export class AppExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Unmapped error.';
+
+    response.status(status).json({ message });
+  }
+}
