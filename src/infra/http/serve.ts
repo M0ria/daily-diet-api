da8c97f7b94b9ccd6364981ab12b40f4c4a52715
Maria <mariacastroferreira17@gmail.com>
2025-04-01 17:36:49 -0300

    Refactor application structure: add AppModule with configuration and scheduling, remove old app and serve files

--- src/infra/http/serve.ts
+++
@@ -1,9 +0,0 @@
-import { env } from '../../config/environment'
-import { app } from './app'
-
-
-const PORT = env.PORT
-
-app.listen(PORT, () => {
-    console.log('[Application] HTTP Server Running on', PORT)
-})
